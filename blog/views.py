92efe9811b0305dbbf5bddd80aebff5c7e04189c
fadel07 <fadel.abbadi@hotmail.com>
2022-04-27 18:48:11 +0300

    databases management

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMs',
-        'title': 'Blog post 1',
-        'content': 'first post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'fadel',
-        'title': 'Blog post 2',
-        'content': '2nd post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context )
 
