d905a9559b4fbdc927b6e7b949e8a226baf69417
fadel07 <fadel.abbadi@hotmail.com>
2022-05-01 17:21:35 +0300

    version 1.0 of project uploaded

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -130,3 +130,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+Email_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+Email_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'django.blog2121@gmail.com'
+EMAIL_HOST_PASSWORD = 'yallahdjango'
